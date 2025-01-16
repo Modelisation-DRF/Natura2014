1948bdd8944977720687a5f5f746ed299bc15b1e
IsabelleAuger <isabelle.auger@mffp.gouv.qc.ca>
2025-01-16 08:11:58 -0500

    Correction fonction genere_graphique

--- R/Graphique.R
+++ R/Graphique.R
@@ -12,31 +12,32 @@
 #' @param Espece Une chaine de caracteres specifiant l'espece d'arbre pour
 #' laquelle generer le graphique. Les valeurs possibles incluent
 #'  "tot" (toutes essences),
-#'  "bop" (bouleau a papier),
-#'  "peu" (peupliers),
+#'  "fi" (feuillus intolerants),
 #'  "ft" (feuillus tolerants),
-#'  "sab" (sapin baumier),
-#'  "epn" (epinette noire),
-#'  "epx" (autres epinettes),
 #'  "ri" (resineux intolerants),
-#'  "rt" (autres resineux tolerants).
+#'  "rt" (resineux tolerants).
+#'  "sab" (sapin baumier),
 #'  La valeur par defaut est "tot".
 #' @param Variable Une chaine de caracteres specifiant la variable a tracer.
 #' Les valeurs possibles incluent
 #'  "st" (surface terriere marchande en m2/ha),
 #'  "n" (nombre d'arbres marchands par ha), "
-#'  v" (volume marchand en m3/ha),
+#'  "v" (volume marchand en m3/ha),
 #'  "hd" (hauteur dominante en m),
 #'  "dq" (diametre quadratique moyen en cm).
 #'  La valeur par defaut est "st".
-#' @param listePlacette Une liste ou un vecteur contenant les identifiants des
-#' placettes a inclure dans le graphique.
 #'
 #' @return Un objet ggplot representant le graphique d'evolution de la variable
 #' specifiee sur la periode d'annee
 #'
 #' @export
-generer_graphique <- function(Data, Espece = "tot", Variable = "st", listePlacette) {
+#'
+#' @import data.table
+#'
+generer_graphique <- function(Data, Espece = "tot", Variable = "st") {
+
+  # Data=data_simul; Espece = "tot"; Variable = "dq";
+
   var <- paste0(paste(Variable), paste(Espece))
 
   # Ensure the variable column exists and is numeric
@@ -51,7 +52,7 @@ generer_graphique <- function(Data, Espece = "tot", Variable = "st", listePlacet
       Etiquette <- "Volume marchand (m3/ha)"
     },
     "hd" = {
-      var <- "hd"
+      var <- "hd"  # on change hdtot pour hd
       Etiquette <- "Hauteur dominante (m)"
     },
     "dq" = {
@@ -64,13 +65,16 @@ generer_graphique <- function(Data, Espece = "tot", Variable = "st", listePlacet
     Data[[var]] <- 0
   }
 
+  # ceci était fait dans natura3 pour faire la moyenne quand la simulation était stochastique, mais natura-2014 n'est pas stochastique, donc pas utile
+  # et si le fihcier est gros, on prend du temps pour rien
   Data <- Data %>%
-    group_by(placette, annee) %>%
-    summarise(mean_value = mean(get(var), na.rm = TRUE), .groups = "drop") %>%
-    ungroup()
+    mutate(mean_value = get(var))
+    #group_by(placette, annee) %>%
+    #summarise(mean_value = mean(get(var), na.rm = TRUE), .groups = "drop") %>%
+    #ungroup()
 
-  anneeMin <- min(Data$annee, na.rm = TRUE)
-  anneeMax <- max(Data$annee, na.rm = TRUE)
+  anneeMin <- min(Data$age, na.rm = TRUE)
+  anneeMax <- max(Data$age, na.rm = TRUE)
 
   # Calculate ymax only if there are valid numeric values
   if (any(!is.na(Data$mean_value))) {
@@ -82,26 +86,26 @@ generer_graphique <- function(Data, Espece = "tot", Variable = "st", listePlacet
 
   Essence <- switch(Espece,
     "tot" = "Toutes essences",
-    "bop" = "Bouleau a papier",
-    "peu" = "Peupliers",
+    "fi" = "Feuillus intolerants",
     "ft" = "Feuillus tolerants",
-    "sab" = "Sapin baumier",
-    "epn" = "epinette noire",
-    "epx" = "Autres epinettes",
     "ri" = "Resineux intolerants",
-    "rt" = "Autres resineux tolerants",
+    "rt" = "Resineux tolerants",
+    "sab" = "Sapin baumier",
     "Toutes essences"
   )
 
-  dernieres_valeurs <- Data %>%
-    group_by(placette) %>%
-    slice(n()) %>%
-    ungroup()
+  # je ne veux pas faire affichier le nopm de placettes sur le graphique
+  # dernieres_valeurs <- Data %>%
+  #   group_by(placette) %>%
+  #   slice(n()) %>%
+  #   ungroup()
+  # en data.table
 
-  GraphEvol <- ggplot(data = Data, aes(x = annee, y = mean_value, group = placette, label = placette)) +
+
+  GraphEvol <- ggplot(data = Data, aes(x = age, y = mean_value, group = placette, label = placette)) +
     geom_line(show.legend = FALSE, lwd = 1.25, colour = "#008000") +
     ylim(0, ifelse(is.na(ymax), 5, ymax + 5)) +
-    xlab(bquote(bold("Annees depuis la perturbation"))) +
+    xlab(bquote(bold("Age moyen du peuplement (ans)"))) +
     ylab(Etiquette) +
     scale_x_continuous(breaks = seq(anneeMin, anneeMax, by = 5)) +
     ggtitle(paste(Etiquette, "  ", Essence)) +
@@ -111,8 +115,8 @@ generer_graphique <- function(Data, Espece = "tot", Variable = "st", listePlacet
       axis.title = element_text(size = 14, face = "bold"),
       strip.text.x = element_text(size = 12, face = "bold"),
       plot.title = element_text(hjust = 0.5, size = 14, face = "bold")
-    ) +
-    geom_text(data = dernieres_valeurs, aes(label = placette), hjust = 1, vjust = -0.2, size = 3)
+    ) #+
+    #geom_text(data = dernieres_valeurs, aes(label = placette), hjust = 1, vjust = -0.2, size = 3)
 
   return(GraphEvol)
 }
